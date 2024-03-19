2a4fb0ef0ed8cded888907c0798f7fe0d85957a8
TuriniMattia <turinimattia96@gmail.com>
2024-03-19 10:16:25 +0100

    [add]pariedispari

--- pari-dispari/app.js
+++ pari-dispari/app.js
@@ -1,9 +1,25 @@
-console.log("pari o dispari")
+const sceltaUtente = prompt("scrivi pari o dispari")
+console.log(sceltaUtente)
+
+const numeroUtente = parseInt(prompt("inserisci un numero da 1 a 5 "))
+console.log(numeroUtente)
+
+const numeroPc = getRandomIntInclusive(1, 5)
+console.log(numeroPc)
+
+const somma = numeroUtente + numeroPc
+console.log(somma, somma % 2)
 
 
 
-function getRandimInt(min, max){
-    const random = Math.floor(Math.random() * (max - min)) + min;
+
+function getRandomIntInclusive(min, max) {
+ const minCeiled = Math.ceil(min)
+ const maxFloored = Math.floor(max)
+ return Math.floor(Math.random () * (maxFloored - minCeiled + 1) + minCeiled)
+
 }
 
-Math.random() * 51
+function isEven(num){
+    return num % 2 === 0
+}
